9479e2ed15a3b90b236a775d0dc5b3f10f7a2f7f
hustccc <1276675421@qq.com>
2021-03-04 01:26:34 +0900

    finish merge two lists

--- src/problems/mod.rs
+++ src/problems/mod.rs
@@ -12,5 +12,9 @@ impl Problems {
     pub fn middle_node(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
         todo!()
     }
+
+    pub fn merge_two_lists(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        todo!()   
+    }
 }
 
--- src/solutions/mod.rs
+++ src/solutions/mod.rs
@@ -64,4 +64,31 @@ impl Solutions {
         }
         slow
     }
+
+    pub fn merge_two_lists(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        let mut res_head = Box::new(ListNode::new(0));
+        let mut current = res_head.as_mut();
+        let mut l1_cur = l1;
+        let mut l2_cur = l2;
+        while l1_cur.is_some() && l2_cur.is_some() {
+            if let (Some(mut l1_head), Some(mut l2_head)) = (l1_cur.take(), l2_cur.take()) {
+                if l1_head.val < l2_head.val {
+                    l1_cur = l1_head.next.take();
+                    l2_cur = Some(l2_head);
+                    current = current.next.get_or_insert(l1_head);
+                } else {
+                    l2_cur = l2_head.next.take();
+                    l1_cur = Some(l1_head);
+                    current = current.next.get_or_insert(l2_head);
+                }
+            }
+        }
+        if l1_cur.is_some() {
+            current.next = l1_cur;
+        }
+        if l2_cur.is_some() {
+            current.next = l2_cur;
+        }
+        res_head.next
+    }
 }
